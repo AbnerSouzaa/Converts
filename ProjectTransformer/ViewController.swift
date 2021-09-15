2a66c52ee82877670a896f03b1656e5f382bfc7f
Abner Souzaa <abnersouzaoppo55@gmail.com>
2021-09-14 21:41:22 -0300

    1.0

---
+++ ProjectTransformer/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  ProjectTransformer
+//
+//  Created by Abner Souza on 14/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var btUnit2: UIButton!
+    @IBOutlet weak var tfValue: UITextField!
+    @IBOutlet weak var btUnit1: UIButton!
+    @IBOutlet weak var lbResult: UILabel!
+    @IBOutlet weak var lbResultUnit: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func ShowNext(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func showNext(_ sender: UIButton) {
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
+
